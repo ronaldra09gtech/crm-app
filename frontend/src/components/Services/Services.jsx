7a540b36dee90425aa52f9f775c43da198d33cc7
Ronald Figueroa <ronaldra09gtech@gmail.com>
2024-12-01 19:50:01 +0800

    added Services Icon

--- frontend/src/components/Services/Services.jsx
+++ frontend/src/components/Services/Services.jsx
@@ -7,25 +7,30 @@ import {
     Row,
     Col,
 } from 'react-bootstrap'
+import designIcon from '../../asset/image/design.png';
+import ecommerceIcon from '../../asset/image/ecommerce.png';
+import webIcon from '../../asset/image/web.png';
 
 export class Services extends Component {
   render() {
     return (
       <Fragment>
         <Container>
-            <Row>
-                <Col>
-                    <h1>One</h1>
+            <Row> 
+                <Col lg={4} md={6} sm={12}>
+                    <div>
+                        <img className="designIcon"src={designIcon}/>
+                    </div>
                 </Col>
-            </Row>
-            <Row>
-                <Col>
-                    <h1>Two</h1>
+                <Col lg={4} md={6} sm={12}>
+                    <div>
+                        <img className="ecommerceIcon" src={ecommerceIcon}/>
+                    </div>
                 </Col>
-            </Row>
-            <Row>
-                <Col>
-                    <h1>Three</h1>
+                <Col lg={4} md={6} sm={12}>
+                    <div>
+                        <img className="webIcon" src={webIcon}/>
+                    </div>
                 </Col>
             </Row>
         </Container>
