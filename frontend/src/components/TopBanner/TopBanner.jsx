a0c51a81a981ee1504a84ac65f729f399d0e8077
Ronald Figueroa <ronaldra09gtech@gmail.com>
2024-12-01 17:04:19 +0800

    created top banner

---
+++ frontend/src/components/TopBanner/TopBanner.jsx
@@ -0,0 +1,22 @@
+import React, { 
+    Component, 
+    Fragment 
+} from 'react'
+import { 
+    Container 
+} from 'react-bootstrap'
+import '../../asset/css/custom.css'
+
+export class TopBanner extends Component {
+  render() {
+    return (
+      <Fragment>
+        <Container className="topFixedBanner">
+          
+        </Container>
+      </Fragment>
+    )
+  }
+}
+
+export default TopBanner
